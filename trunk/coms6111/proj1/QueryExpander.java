de686a33b3eb025998da98d4f3b79563fe8e7f97
talklittle <talklittle@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-10 21:50:37 +0000

    [initial] Initial skeleton code doesn't do anything

---
+++ trunk/coms6111/proj1/QueryExpander.java
@@ -0,0 +1,5 @@
+package coms6111.proj1;
+
+public interface QueryExpander {
+	// TODO I don't know what goes here yet
+}
---
+++ trunk/coms6111/proj1/Resultset.java
@@ -0,0 +1,12 @@
+package coms6111.proj1;
+
+public class Resultset {
+	private Query myQuery;
+	private Result[] myResults;
+	private int resultSize;
+	
+	
+	public Resultset() {
+		// TODO
+	}
+}
