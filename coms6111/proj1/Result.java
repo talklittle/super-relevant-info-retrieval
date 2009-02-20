6637d4d51a8f75dcdaff4190d34fd19472fda163
talklittle <talklittle@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-20 20:48:41 +0000

    [yahoo] Execute a query on yahoo and get raw result
    Debugging: prints out the raw result

--- coms6111/proj1/Result.java
+++ coms6111/proj1/Result.java
@@ -1,11 +1,28 @@
 package coms6111.proj1;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 /**
  * Represents one result as returned by search engine
  */
 public class Result {
+	private static Log log = LogFactory.getLog(Result.class);
+	
 	public String title, summary;
 	public URL url;
+	
+	public Result(String theTitle, String theSummary, String theUrl) {
+		title = theTitle;
+		summary = theSummary;
+		try {
+			url = new URL(theUrl);
+		} catch (MalformedURLException e) {
+			log.warn(e);
+			url = null;
+		}
+	}
 }
--- coms6111/proj1/Resultset.java
+++ coms6111/proj1/Resultset.java
@@ -1,19 +1,60 @@
 package coms6111.proj1;
 
 import java.io.*;
+import java.util.*;
+
+import javax.xml.xpath.*;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.w3c.dom.*;
 
 /**
  * A Resultset from search engine
  */
 public class Resultset {
+	private static Log log = LogFactory.getLog(Resultset.class);
+	
 	private Query myQuery;
-	private Result[] myResults;
+	private List<Result> myResults;
 	private int resultSize;
 	
 	/**
 	 * Constructor using input stream returned from HttpClient
 	 */
-	public Resultset(InputStream rstream) {
+	public Resultset(InputStream rstream) throws Exception {
+		Document response = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(rstream);
 		
+		XPathFactory factory = XPathFactory.newInstance();
+		XPath xPath=factory.newXPath();
+
+		//Get all search Result nodes
+		NodeList nodes = (NodeList)xPath.evaluate("/ResultSet/Result", response, XPathConstants.NODESET);
+		int nodeCount = nodes.getLength();
+        
+		myResults = new ArrayList<Result>();
+		//iterate over search Result nodes
+		for (int i = 0; i < nodeCount; i++) {
+			//Get each xpath expression as a string
+			String title = (String)xPath.evaluate("Title", nodes.item(i), XPathConstants.STRING);
+			String summary = (String)xPath.evaluate("Summary", nodes.item(i), XPathConstants.STRING);
+			String url = (String)xPath.evaluate("Url", nodes.item(i), XPathConstants.STRING);
+			//print out the Title, Summary, and URL for each search result
+			log.trace("Title: " + title);
+			log.trace("Summary: " + summary);
+			log.trace("URL: " + url);
+			log.trace("--");
+			
+			myResults.add(new Result(title, summary, url));
+		}
+	}
+	
+	public Iterator<Result> getIterator() {
+		return myResults.iterator();
+	}
+	public ListIterator<Result> getListIterator() {
+		return myResults.listIterator();
 	}
 }
