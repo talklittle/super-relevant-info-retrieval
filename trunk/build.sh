7897c50728ee96fb2bb44dfbec7ec4c17934860c
talklittle <talklittle@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-22 14:47:00 +0000

    [dependencies] Add lucene jar, build.sh, license files

---
+++ trunk/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+PWD=`pwd`
+LIBDIR=$PWD/src/lib
+javac -classpath "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$LIBDIR/lucene-2.4.0/lucene-core-2.4.0.jar" -d $PWD/bin/ $PWD/src/coms6111/proj1/*.java
