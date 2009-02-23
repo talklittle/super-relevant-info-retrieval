507693037cc795f0a77636fe2aa5ebbc681fd02b
biran1221 <biran1221@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-23 03:56:12 +0000

    [term frequency] Finding term frequency
    Create indexes and find term frequency for relevant and nonrelevant summaries

--- src/coms6111/proj1/QueryExpander.java
+++ src/coms6111/proj1/QueryExpander.java
@@ -5,5 +5,6 @@ package coms6111.proj1;
  * A QueryExpander is a class that expands a Query by looking at the relevant Resultset
  */
 public interface QueryExpander {
-	public Query apply(Query query, Resultset relevantResultset);
+	public Query apply(Query query, Resultset relevantResultset,
+			Resultset nonrelevantResultset);
 }
