d449fca66b4c69f3ffac1c307ee48f0ac8475381
talklittle <talklittle@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-25 22:14:19 +0000

    [gui] Improve GUI. 1024x768 recommended
    - Added column headings
    - Added mnemonics
    - Set preferred size 1024x768

--- src/coms6111/proj1/RunnerGUI.java
+++ src/coms6111/proj1/RunnerGUI.java
@@ -66,31 +66,58 @@ public class RunnerGUI extends JFrame {
 	 * @param rs New Resultset to replace current one
 	 */
 	public void setResultset(Resultset rs) {
-		Iterator<Result> it = rs.iterator();
-		int i = 0;
-		
 		visibleResultset = rs;
 		cbList = new ArrayList<JCheckBox>();
 		
-		// Add the Resultset to the GUI
 		resultsPanel.removeAll();
 		GridBagConstraints c = new GridBagConstraints();
+		
+		// Display current query at top
+		JTextArea currentQueryTextArea = new JTextArea("Current query: " + currentQuery.toString());
+		currentQueryTextArea.setBackground(Color.YELLOW);
+		currentQueryTextArea.setEditable(false);
+		c.fill = GridBagConstraints.NONE;
+		c.gridwidth = 4;
+		c.gridx = 0;
+		c.gridy = 0;
+		resultsPanel.add(currentQueryTextArea, c);
+		
+		// Headers
+		JLabel cbHeader = new JLabel("<html><u>Relevant?</u></html>");
+		c.gridwidth = 1;
+		c.gridx = 0;
+		c.gridy = 1;
+		resultsPanel.add(cbHeader, c);
+		JLabel titleHeader = new JLabel("<html><u>Title</u></html>");
+		c.gridx = 1;
+		resultsPanel.add(titleHeader, c);
+		JLabel summaryHeader = new JLabel("<html><u>Summary</u></html>");
+		c.gridx = 2;
+		resultsPanel.add(summaryHeader, c);
+		JLabel urlHeader = new JLabel("<html><u>URL</u></html>");
+		c.gridx = 3;
+		resultsPanel.add(urlHeader, c);
+		
+		// Add each result of the Resultset to the GUI
+		Iterator<Result> it = rs.iterator();
+		int resultsPanelRow = 2, resultIndex = 1;
 		while (it.hasNext()) {
 			Result r = (Result) it.next();
 			Color bgcolor;
-			if ((i & 0x1) == 0)
+			if ((resultsPanelRow & 0x1) == 0)
 				bgcolor = Color.decode("0xe7e7e7");
 			else
 				bgcolor = Color.WHITE;
 			
 			JCheckBox cb = new JCheckBox();
 			cbList.add(cb);
-			cb.setText(""+(i+1));
+			cb.setText(""+resultIndex);
 			cb.setBackground(bgcolor);
 			cb.setSelected(true);
+			cb.setMnemonic(0x30 + (resultIndex % 10)); // ASCII '0' thru '9'
 			c.fill = GridBagConstraints.BOTH;
 			c.gridx = 0;
-			c.gridy = i;
+			c.gridy = resultsPanelRow;
 			resultsPanel.add(cb, c);
 			
 			JTextArea titleTextArea = 
@@ -101,8 +128,10 @@ public class RunnerGUI extends JFrame {
 			titleTextArea.setLineWrap(true);
 			titleTextArea.setBackground(bgcolor);
 			titleTextArea.setOpaque(true);
+//			titleTextArea.setColumns(20);
+			c.weightx = 0.25;
 			c.gridx = 1;
-			c.gridy = i;
+			c.gridy = resultsPanelRow;
 			resultsPanel.add(titleTextArea, c);
 			
 			JTextArea summaryTextArea =
@@ -111,9 +140,10 @@ public class RunnerGUI extends JFrame {
 			summaryTextArea.setEditable(false);
 			summaryTextArea.setWrapStyleWord(true);
 			summaryTextArea.setLineWrap(true);
-			summaryTextArea.setColumns(40);
+//			summaryTextArea.setColumns(40);
+			c.weightx = 0.75;
 			c.gridx = 2;
-			c.gridy = i;
+			c.gridy = resultsPanelRow;
 			resultsPanel.add(summaryTextArea, c);
 			
 			// TODO make this clickable
@@ -123,17 +153,20 @@ public class RunnerGUI extends JFrame {
 			urlTextArea.setFont(new Font(null, Font.BOLD, 12));
 			urlTextArea.setLineWrap(true);
 			urlTextArea.setOpaque(true);
-			urlTextArea.setColumns(20);
+//			urlTextArea.setColumns(20);
+			c.weightx = 0.25;
 			c.gridx = 3;
-			c.gridy = i;
+			c.gridy = resultsPanelRow;
 			resultsPanel.add(urlTextArea, c);
 			
-			i++;
+			resultsPanelRow++;
+			resultIndex++;
 		}
 		
 		JButton expandButton = new JButton("Expand query & Refine results");
+		expandButton.setMnemonic(KeyEvent.VK_E);
 		expandButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent ev) {
 				List<Result> relevantResults = new ArrayList<Result>();
 				List<Result> nonrelevantResults = new ArrayList<Result>();
 				Iterator<JCheckBox> itCb = cbList.iterator();
@@ -177,10 +210,11 @@ public class RunnerGUI extends JFrame {
 		});
 		c.fill = GridBagConstraints.NONE;
 		c.gridx = 0;
-		c.gridy = i;
+		c.gridy = resultsPanelRow;
 		c.gridwidth = 4;
 		resultsPanel.add(expandButton, c);
-	
+		resultsPanel.repaint();
+		
 		frame.pack();
 		frame.pack(); // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6223727
 	}
@@ -232,7 +266,7 @@ public class RunnerGUI extends JFrame {
         inputPanel.add(targetPrecisionLabel, c);
 		
 		// Submit button
-        submitButton = new JButton("Submit");
+        submitButton = new JButton("Submit initial query");
 		submitButton.addActionListener(new QueryActionListener());
 		c.fill = GridBagConstraints.NONE;
 		c.gridwidth = 2;
@@ -241,10 +275,14 @@ public class RunnerGUI extends JFrame {
 		inputPanel.add(submitButton, c);
 		
 		// Other buttons -- "About" and "Exit"
-		JPanel otherPanel = new JPanel();
+		JPanel otherPanel = new JPanel(new FlowLayout());
+		JLabel hintLabel = new JLabel("<html><em>Hint: Use Alt+[0-9] for faster selection of checkboxes</em></html>");
+		otherPanel.add(hintLabel);
+//		c.gridx = 0;
+//		c.gridy = 0;
 		JButton aboutButton = new JButton("About");
 		aboutButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent ev) {
 				JOptionPane.showMessageDialog(null,
 						"Authors:\n"
 						+ "Ran Bi <rb2651@columbia.edu>\n"
@@ -254,14 +292,18 @@ public class RunnerGUI extends JFrame {
 						+ "Spring 2009");
 			}
 		});
+//		c.gridx = 0;
+//		c.gridy = 1;
 		otherPanel.add(aboutButton);
 		
 		JButton exitButton = new JButton("Exit");
+		exitButton.setMnemonic(KeyEvent.VK_X);
 		exitButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+			public void actionPerformed(ActionEvent ev) {
 				System.exit(0);
 			}
 		});
+//		c.gridx = 1;
 		otherPanel.add(exitButton);
 		
 		resultsPanel = new JPanel(new GridBagLayout());
@@ -270,11 +312,11 @@ public class RunnerGUI extends JFrame {
 		
 		// Add stuff to container
 		Container content = this.getContentPane();
-		content.setLayout(new BorderLayout());
+		content.setLayout(new BorderLayout(0,0));
+		content.setPreferredSize(new Dimension(1024,0));
 		content.add(inputPanel, BorderLayout.NORTH);
 		content.add(resultsScrollPane, BorderLayout.CENTER);
 		content.add(otherPanel, BorderLayout.SOUTH);
-
 		this.pack();
 		this.setVisible(true);
 	}
@@ -299,7 +341,7 @@ public class RunnerGUI extends JFrame {
      * Listener for pressing return or button click. Executes query.
      */
 	private class QueryActionListener implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
+		public void actionPerformed(ActionEvent ev) {
 			currentQuery = new Query(queryTextField.getText());
 			executeCurrentQuery();
 		}
@@ -311,6 +353,7 @@ public class RunnerGUI extends JFrame {
 	 */
 	public static void main(String[] args) {
 		frame = new RunnerGUI("Super Relevant Info Retrieval");
+		frame.setPreferredSize(new Dimension(1024,768));
 		frame.init();
 	}
 }
