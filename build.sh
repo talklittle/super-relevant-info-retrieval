803cff47aa62f6956f09735cb159e635b60e4d68
talklittle <talklittle@95a20644-f256-11dd-a8e2-f7cb3c35fcff>
2009-02-26 05:24:53 +0000

    [final] Everything finalized to turn in. Just need to create tar.gz.

--- build.sh
+++ build.sh
@@ -12,4 +12,6 @@ else
     OUTDIR="$1"
 fi
 
-javac -classpath "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$LIBDIR/lucene-2.4.0/lucene-core-2.4.0.jar" -d "$OUTDIR" $PWD/src/coms6111/proj1/*.java
+javac -classpath "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$LIBDIR/lucene-2.4.0/lucene-core-2.4.0.jar:$PWD/src" -d "$OUTDIR" $PWD/src/coms6111/proj1/*.java
+
+cp src/log4j.properties bin/
--- run.sh
+++ run.sh
@@ -18,4 +18,4 @@ if [ ! -d "./bin" ]; then
 fi
 cd ./bin
 
-java -cp "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$LIBDIR/lucene-2.4.0/lucene-core-2.4.0.jar" coms6111/proj1/RunnerGUI
+java -cp "$CLASSPATH:$LIBDIR/junit-4.5.jar:$LIBDIR/commons-codec-1.3/commons-codec-1.3.jar:$LIBDIR/commons-httpclient-3.1/commons-httpclient-3.1.jar:$LIBDIR/commons-logging-1.1.1/commons-logging-1.1.1.jar:$LIBDIR/commons-lang-2.4/commons-lang-2.4.jar:$LIBDIR/apache-log4j-1.2.15/log4j-1.2.15.jar:$LIBDIR/lucene-2.4.0/lucene-core-2.4.0.jar:." coms6111/proj1/RunnerGUI
